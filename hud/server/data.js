665cf80a5c12e41992790e8048f059ee51d3c7be
kitajchuk <kitajchuk@gmail.com>
2018-01-27 19:10:32 -0800

    data refactor

---
+++ hud/server/data.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "hearts": {
+        "max": 18,
+        "value": 18,
+        "heartThiefs": [],
+        "fairyFinders": [],
+        "fairyBottles": []
+    },
+    "fairies": {
+        "max": 15,
+        "value": 0
+    }
+};
